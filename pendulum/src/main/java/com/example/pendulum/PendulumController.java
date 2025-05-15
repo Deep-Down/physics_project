e1db4cdaac4ec79653dc6620ecac72f327d748e1
unknown <leeyongmine@gmail.com>
2025-05-15 16:19:46 +0800

    update

--- pendulum/src/main/java/com/example/pendulum/PendulumController.java
+++ pendulum/src/main/java/com/example/pendulum/PendulumController.java
@@ -12,64 +12,68 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PendulumController {
 
-    public static class PendulumRequest {
-        private double length;
-        private double angle;
+@org.springframework.beans.factory.annotation.Autowired
+    private Simulation simulation;
 
-        public double getLength() {
-            return length;
-        }
+// Setter-инъекция
 
-        public void setLength(double length) {
-            this.length = length;
-        }
+public static class PendulumRequest {
+    private double length;
+    private double angle;
 
-        public double getAngle() {
-            return angle;
-        }
-
-        public void setAngle(double angle) {
-            this.angle = angle;
-        }
+    public double getLength() {
+        return length;
     }
 
-    public static class PendulumResponse {
-        public double period;
-        public double gravity;
-
-        public PendulumResponse(double period, double gravity) {
-            this.period = period;
-            this.gravity = gravity;
-        }
-
-        public double getPeriod() {
-            return period;
-        }
-
-        public double getGravity() {
-            return gravity;
-        }
+    public void setLength(double length) {
+        this.length = length;
     }
 
-    // GET-запрос для отображения страницы
-    @GetMapping("/pendulum")
-    public String showPendulumPage() {
-        return "pendulum"; // Возвращаем название HTML-шаблона
+    public double getAngle() {
+        return angle;
     }
 
-
-    @PostMapping("/pendulum")
-    @ResponseBody
-    public PendulumResponse calculatePendulum(@RequestBody PendulumRequest request) {
-        double length = request.getLength();
-        double angle = request.getAngle();
-        double initialG = 9.8;
-
-        Simulation simulation = new Simulation(length, angle, initialG);
-        double period = simulation.getCurrentPeriod();
-        double gravity = simulation.refineGravity(period);
-        log.info("POST request processed. Period = {}", period);
-        return new PendulumResponse(period, gravity);
+    public void setAngle(double angle) {
+        this.angle = angle;
     }
+}
+
+public static class PendulumResponse {
+    private double period;
+    private double gravity;
+
+    public PendulumResponse(double period, double gravity) {
+        this.period = period;
+        this.gravity = gravity;
+    }
+
+    public double getPeriod() {
+        return period;
+    }
+
+    public double getGravity() {
+        return gravity;
+    }
+}
+
+@GetMapping("/pendulum")
+public String showPendulumPage() {
+    return "pendulum";
+}
+
+@PostMapping("/pendulum")
+@ResponseBody
+public PendulumResponse calculatePendulum(@RequestBody PendulumRequest request) {
+    double length = request.getLength();
+    double angle = request.getAngle();
+    double initialG = 9.8;
+
+    simulation.initialize(length, angle, initialG);
+    double period = simulation.getCurrentPeriod();
+    double gravity = simulation.refineGravity(period);
+
+    log.info("POST request processed. Period = {}", period);
+    return new PendulumResponse(period, gravity);
+}
 }
 
--- pendulum/src/main/java/com/example/pendulum/Simulation.java
+++ pendulum/src/main/java/com/example/pendulum/Simulation.java
@@ -1,34 +1,36 @@
 package com.example.pendulum;
 
+import org.springframework.stereotype.Component;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class Simulation {
     private double length;
     private double angle;
     private double gravity;
 
-    public Simulation(double length, double angle, double gravity) {
-        if (length <= 0 || angle < 0 || angle > 90) {
-            throw new IllegalArgumentException("Invalid parameters.");
-        }
-        this.length = length;
-        this.angle = angle;
-        this.gravity = gravity;
+public void initialize(double length, double angle, double gravity) {
+    if (length <= 0 || angle < 0 || angle > 90) {
+        throw new IllegalArgumentException("Invalid parameters.");
     }
+    this.length = length;
+    this.angle = angle;
+    this.gravity = gravity;
+}
 
-    public double getCurrentPeriod() {
-        double thetaRad = Math.toRadians(angle);
-        double correction = 1 + Math.pow(thetaRad, 2) / 16.0;
-        double period = 2 * Math.PI * Math.sqrt(length / gravity) * correction;
-        log.info("Calculated period: {}", period);
-        return period;
-    }
-    
+public double getCurrentPeriod() {
+    double thetaRad = Math.toRadians(angle);
+    double correction = 1 + Math.pow(thetaRad, 2) / 16.0;
+    double period = 2 * Math.PI * Math.sqrt(length / gravity) * correction;
+    log.info("Calculated period: {}", period);
+    return period;
+}
 
-    public double refineGravity(double measuredPeriod) {
-        return 4 * Math.PI * Math.PI * length / (measuredPeriod * measuredPeriod);
-    }
+public double refineGravity(double measuredPeriod) {
+    return 4 * Math.PI * Math.PI * length / (measuredPeriod * measuredPeriod);
+}
 }
 
 
